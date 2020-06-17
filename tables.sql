231a51b893d9421e153278de901fff12a8ff723b
Litvinov Ilya <litvinov374@gmail.com>
2020-06-17 14:30:15 +0400

    Intial project

---
+++ tables.sql
@@ -0,0 +1,19 @@
+create table users (
+    user_id int auto_increment,
+    user_login varchar(30) NOT NULL ,
+    user_password mediumtext NOT NULL,
+    constraint uploaded_pk
+    primary key (user_id)
+)engine = INNODB
+ character set utf8
+ collate utf8_general_ci;
+
+ create table images (
+    img_id int auto_increment NOT NULL,
+    img_name varchar(255) NOT NULL ,
+    uploaded_user_id int,
+    constraint images_pk
+    primary key (img_id)
+)engine = INNODB
+ character set utf8
+ collate utf8_general_ci;
